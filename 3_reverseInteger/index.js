70f8b789e2e7ef74e4014171ce361e362a9162e1
RahatRezwan <kazi.rahat.rezwan889@gmail.com>
2024-06-23 17:48:15 +0600

    solve reverse integer problem

--- 3_reverseInteger/index.js
+++ 3_reverseInteger/index.js
@@ -8,14 +8,26 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
+// solution-2
 function reverseInt(n) {
-  const reversed = n
-    .toString()
-    .split('')
-    .reverse()
-    .join('');
-
-  return parseInt(reversed) * Math.sign(n);
+  let reversedNumber = 0;
+  while (n !== 0) {
+    const lastNumber = n % 10;
+    reversedNumber = reversedNumber * 10 + lastNumber;
+    n = (n - lastNumber) / 10;
+  }
+  return reversedNumber;
 }
 
 module.exports = reverseInt;
+
+// solution-1
+// function reverseInt(n) {
+//   const reversed = n
+//     .toString()
+//     .split('')
+//     .reverse()
+//     .join('');
+
+//   return parseInt(reversed) * Math.sign(n);
+// }
